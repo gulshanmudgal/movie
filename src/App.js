6c6d5105661d19ffd4b96926cdeb68399c118484
Gulshan Kumar <inbox.gulshan@outlook.com>
2023-06-11 13:13:56 +0530

    created store using redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.scss";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import './App.scss';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="app">
+      <Router>
+        <Header></Header>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" Component={Home} />
+            <Route exact path="/movie/:imdbID" Component={MovieDetail} />
+            <Route path="*" Component={PageNotFound} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
